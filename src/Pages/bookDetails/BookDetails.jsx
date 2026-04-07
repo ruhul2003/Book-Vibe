233e893c70df9209733042468c6c87ba52fa8cf8
ruhul2003 <ruhul941020@gmail.com>
2026-04-07 22:47:33 +0600

    Added all dynamic informations

--- src/Pages/bookDetails/BookDetails.jsx
+++ src/Pages/bookDetails/BookDetails.jsx
@@ -19,10 +19,25 @@ const BookDetails = () => {
                     src={expectedBook.image} className='h-[400px] ' />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h2 className="card-title">{expectedBook.bookName}</h2>
+                <p>{expectedBook.author}</p>
+                <p>{expectedBook.category}</p>
+                <p>{expectedBook.description}</p>
+                <div>
+                    {expectedBook.tags.map((tag) => (
+                        <div key={tag} className="badge text-green-700 bg-green-100 font-semibold text-xs">
+                            {tag}
+                        </div>
+                    ))}
+                </div>
+                <p>Number of pages: {expectedBook.totalPages}</p>
+                <p>Publisher : {expectedBook.publisher}</p>
+                <p>Year of Publication: {expectedBook.yearOfPublishing}</p>
+                <p>Rating : {expectedBook.rating}</p>
+
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Listen</button>
+                    <button className="btn btn-accent">Wishlist</button>
                 </div>
             </div>
         </div>
